06b25fa74ec4b6dc79a48a08a4b52ff5be79c9d0
Adil <madil90@gmail.com>
2017-12-27 00:05:47 -0800

    Added Vehicle detection project

---
+++ self-driving-nanodegree/CarND-Vehicle-Detection/src/vehicle_detector.py
@@ -0,0 +1,27 @@
+import os
+
+# Main class that trains a classifier for 
+# vehicles and predicts using the classifier
+class VehicleDetector:
+    
+    # load the dataset
+    def load_training_set(self):
+        pass
+    
+    # train the detector
+    def train_detector(self):
+        # TODO :
+        # 1) Extract all features
+        # 2) Pre-process data
+        # 3) train_SVM
+        # 4) Save_weights 
+        pass
+    
+    # vehicle detector using windows
+    def detect_vehicles(self):
+        # TODO :
+        # 1) load video or images 
+        # 2) Get features
+        # 3) Run windows 
+        # 4) overlay detections
+        pass 
